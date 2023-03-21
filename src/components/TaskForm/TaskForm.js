7992a06c7bc7b84371bbf51e89667e720f66e20f
alexxx888 <alexxxusachev888@gmail.com>
2023-03-21 14:12:25 +0200

    test repo from sandbox

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,12 +1,17 @@
+import { useDispatch } from "react-redux";
+import { addTask } from "../../redux/actions";
 import { Button } from "components/Button/Button";
 import css from "./TaskForm.module.css";
 
 export const TaskForm = () => {
-  const handleSubmit = (event) => {
+  const dispatch = useDispatch();
+  
+  const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+    dispatch(addTask(form.elements.text.value));
     form.reset();
-  };
+  }
 
   return (
     <form className={css.form} onSubmit={handleSubmit}>
